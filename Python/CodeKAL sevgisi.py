49f1b2a5ce892b9463c93b5d452b192cd981ca13
MasterPriva <42203633+MasterPriva@users.noreply.github.com>
2019-10-23 17:29:39 +0300

    Update CodeKAL sevgisi.py

--- Python/CodeKAL sevgisi.py
+++ Python/CodeKAL sevgisi.py
@@ -7,3 +7,5 @@ print("CodeKAL'ın bir parçası olmaktan çok mutluyum!")
 
 #YAZ: Aynı görevi java programlama dili ile de yaptınız. Bu iki dil arasında gördüğünüz farklar nedir?
 # başına "#" (yorum işareti) koyarak düşüncelerinizi kodun sonuna ekleyiniz.
+print("Me Love CodeClub" + "Me Is Happy To Be CodeClub")
+#java çok uğraştırıcı python sa direk İngilizce gibi, biri daha kolay ama birini yazarken kendini gerçekten programcı gibi hissediyorsun
